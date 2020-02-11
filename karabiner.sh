a509c7d86a5d65b9f38964c4e6f8075b51bbc2f5
Vlad M <vladinflow@gmail.com>
2020-02-12 05:30:51 +0600

    removed layouts because they mess up shortcuts and added tab switching in terminal in webstorm

--- karabiner.sh
+++ karabiner.sh
@@ -5,6 +5,3 @@ xmodmap -e "clear Lock"
 xmodmap -e "keycode 108 = Alt_R"
 xmodmap -e "remove mod1 = Alt_R"
 xmodmap -e "add mod3 = Alt_R"
-
-setxkbmap -layout us,ru
-setxkbmap -option 'grp:alt_shift_toggle'
